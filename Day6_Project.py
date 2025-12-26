b9755e917d599a3d889584645c7141a257a3f78f
R4unak-t4 <thapamagarraunak@gmail.com>
2025-12-26 12:47:11 +0545

    First

---
+++ Day6_Project.py
@@ -0,0 +1,14 @@
+# Grade Calculator
+
+# 1. Take the Score (In int) Input From the User
+
+# 2. Check if the score lies in Grade A, Grade B, Grade C or Fail
+
+# A -> 90-100
+# B -> 80-89
+# C -> 50-79
+# Fail -> ???
+
+# 3. If the score is Invalid display appropriate message
+
+# Optional : Take score input in float
