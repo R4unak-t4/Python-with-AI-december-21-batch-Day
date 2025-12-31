8ca00afc6e127552eeeca3315801a40d1167f5b8
R4unak-t4 <thapamagarraunak@gmail.com>
2025-12-31 12:53:54 +0545

    First

---
+++ Day9.py
@@ -0,0 +1,32 @@
+# Question
+#
+# Write a Python program that:
+#
+# Asks the user how many numbers they want to enter.
+#
+# Uses a loop to take those numbers as input and store them in a list.
+#
+# For each number:
+#
+# Use if–else to check whether the number is positive, negative, or zero.
+#
+# After processing all numbers:
+#
+# Print the list of numbers
+#
+# Print how many numbers are positive, negative, and zero
+#
+# Sample Input
+# Enter how many numbers: 6
+# Enter number 1: 5
+# Enter number 2: -3
+# Enter number 3: 0
+# Enter number 4: 7
+# Enter number 5: -1
+# Enter number 6: 0
+#
+# Sample Output
+# Numbers entered: [5, -3, 0, 7, -1, 0]
+# Positive count: 2
+# Negative count: 2
+# Zero count: 2
