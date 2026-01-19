8e87edf89b62f505bfaaf981958f43b196638c1e
R4unak-t4 <thapamagarraunak@gmail.com>
2026-01-19 12:49:53 +0545

    First

--- Week3/Day4.py
+++ Week3/Day4.py
@@ -20,7 +20,6 @@ bruno.eat()
 
 bird = Animal('bird')
 bird.eat()
-
 '''
 Create a base class called Vehicle with methods:
 
@@ -38,3 +37,33 @@ Bike, which overrides the honk method to print "The bike rings its bell."
 
 Create a function called perform_actions(vehicle) that accepts a Vehicle object as an argument and calls the accelerate(), brake(), and honk() methods on it. The function should work for any Vehicle subclass.
 '''
+
+class Vehicle:
+
+    def accelerate(self):
+        print("The vehicle is accelerating.")
+
+    def brake(self):
+        print("The vehicle is braking.")
+
+    def honk(self):
+        print("The vehicle is honking.")
+
+class Car(Vehicle):
+    def accelerate(self):
+        print("The car is speeding up!")
+
+class Bike(Vehicle):
+    def brake(self):
+        print("The bike rings its bell.")
+
+
+
+bk = Bike()
+
+def perform_actions(vehicle):
+    vehicle.honk()
+    vehicle.brake()
+    vehicle.accelerate()
+
+perform_actions(bk)
